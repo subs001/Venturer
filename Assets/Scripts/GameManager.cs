7db6de21d49c28d86630659eb6ab970788d79148
ritikasarkar18 <ritikasarkar438@gmail.com>
2021-12-01 10:16:57 +0530

    Testing changes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    private static int currentLevel = 0; 
+    private static int numoflevels = 2; /** assuming we have 2 levels **/
+    
+    public static void NewLevel(){
+        if (currentLevel < numoflevels) 
+        {
+            currentLevel += 1;
+            SceneManager.LoadScene(currentLevel);
+        }
+        else 
+        {
+            print("You win"); /** Replace with appropriate stuff**/
+        }
+    }
+
+
+
+    /**
+    Assuming restarts current level
+    **/
+    public static void Restart()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+
+
+
+    /**
+    Assuming restarts from the first level
+    **/
+    public static void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+    public static void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+}
