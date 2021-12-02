84b3d9b94d3b512e1c1695d34898ff51dd2fcf31
Suhail Ahmed <61581649+SuhailAhmedVelorum@users.noreply.github.com>
2021-12-02 11:00:45 +0530

    Shooting disabled when menu is up

--- Assets/Scripts/Menu/MenuCall.cs
+++ Assets/Scripts/Menu/MenuCall.cs
@@ -5,59 +5,67 @@ using UnityEngine.UI;
 
 public class MenuCall : MonoBehaviour
 {
-	public GameObject menu;
-	public PlayerMovement player;
-	private bool isShowing = false;
-	public Slider sens;
+    public GameObject menu;
+    public PlayerMovement player;
+    public ProjectileGun projectileGun;
+    private bool isShowing = false;
+    public Slider sens;
 
-	void Start(){
-		sens.value = player.sensitivity;
-	}
+    void Start()
+    {
+        menu.SetActive(false);
+        sens.value = player.sensitivity;
+    }
 
-	void Update()
-	{
-		player.sensitivity = 50 * sens.value;
-		Debug.Log(player.sensitivity);
-
-		if (Input.GetKeyDown("tab"))
+    void Update()
+    {
+        player.sensitivity = 50 * sens.value;
+        if (Input.GetKeyDown("tab"))
         {
-			StartCoroutine(detectPush());
-        	if (Input.GetKeyDown("tab"))
-        	{
-            	handleButton();
-        	}
+            StartCoroutine(detectPush());
+            if (Input.GetKeyDown("tab"))
+            {
+                handleButton();
+            }
         }
-	}
+    }
 
-	IEnumerator detectPush()
-	{
-		yield return new WaitForSeconds(0.1f);
-	}
+    IEnumerator detectPush()
+    {
+        yield return new WaitForSeconds(0.1f);
+    }
 
-	void handleButton(){
-		if(isShowing){
-			resume();
-		}
-		else {
-			pauseAndMenu();
-		}
-	}
+    void handleButton()
+    {
+        if (isShowing)
+        {
+            resume();
+        }
+        else
+        {
+            pauseAndMenu();
+        }
+    }
 
-	public void pauseAndMenu(){
-		Time.timeScale = 0;
-		Cursor.visible = true;
-		menu.SetActive(true);
-		Cursor.lockState = CursorLockMode.None;
-		isShowing = true;
-		player.lockLook = true;
-	}
+    public void pauseAndMenu()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        menu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        isShowing = true;
+        projectileGun.shootingEnabled = false;
+        player.lockLook = true;
+    }
 
-	public void resume(){
-		Time.timeScale = 1;
-		menu.SetActive(false);
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-		isShowing = false;
-		player.lockLook = false;
-	}
+    public void resume()
+    {
+        Time.timeScale = 1;
+        menu.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        isShowing = false;
+        projectileGun.shootingEnabled = true;
+        player.lockLook = false;
+    }
 }
